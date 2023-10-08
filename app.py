b920905e359e1395b29fb0e33127cc6e0abfa7a2
Daisy-Wang-Dev <107044170+Daisy-Wang-Dev@users.noreply.github.com>
2023-10-08 15:49:00 +0000

    Added connection to db

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-Jobs = [
-    {"id": 1, "title": "Data Analyst", "location": "New York, USA", "salary":"120,000"},
-    {"id": 2, "title": "Software Engineer", "location": "San Francisco, USA", 
-     "salary": "150,000"},
-    {"id": 3, "title": "Marketing Manager", "location": "Los Angeles, USA", 
-     "salary": "100,000"},
-    {"id": 4, "title": "UX Designer", "location": "Chicago, USA", "salary": "110,000"}
-]
 
 # Can treat this like a api endpoint, it just renders html instead of JSON
 @app.route("/")
 def hello_world():
+    Jobs = load_jobs_from_db()
     return render_template("home.html", jobs = Jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
+    Jobs = load_jobs_from_db()
     return jsonify(Jobs)
 
 if __name__ == '__main__':
---
+++ database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine, text
+import sqlalchemy
+import os
+
+db_string = os.environ['DB_CONNECTION_STRING']
+
+engine=create_engine(db_string, 
+ connect_args= {
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem",            
+        }
+    }          
+)
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+
+    jobs = []
+
+    for row in result.all():
+      jobs.append(row._asdict())
+  
+  return jobs
